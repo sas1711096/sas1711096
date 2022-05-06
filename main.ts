2bfc95a3ad52faa1713a0af247f2705ec32a5f4f
sas1711096 <105028853+sas1711096@users.noreply.github.com>
2022-05-06 10:15:16 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1)
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
 })
